abb294327f235bb48c91a23da465dbbdbaea1c89
rico-rt <rico@beyondmood.com>
2026-01-06 15:39:07 -0500

    First init

--- app/supabase.server.js
+++ app/supabase.server.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Vite environment variables
 export const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY // Must use Service Role for backend writes
+    import.meta.env.SUPABASE_URL,
+    import.meta.env.SUPABASE_SERVICE_ROLE_KEY // Must use Service Role for backend writes
 );
